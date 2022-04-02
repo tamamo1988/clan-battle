b0d56d55e81da0783d45f04ab60c8645ae670eb2
tamamo1988 <60849330+tamamo1988@users.noreply.github.com>
2022-04-02 18:44:48 +0900

    Update set.js

--- src/set.js
+++ src/set.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-const request = require('request');
+const request = require('request-promise');
 
 require('dotenv').config();
 const server_url = process.env.SERVER_URL;
