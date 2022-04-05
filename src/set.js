697907c9a57e9bd319a1586e3764fd2c682f2a18
tamamo1988 <60849330+tamamo1988@users.noreply.github.com>
2022-04-05 11:50:58 +0900

    Update set.js

--- src/set.js
+++ src/set.js
@@ -683,6 +683,9 @@ async function Read_File(filename){
 		//await pg.connect();
 		// コネクション取得
 		const connect = await pg.connect();
+		console.log("-------");
+		console.log(connect);
+		console.log("-------");
 		
 		let result = await pg.query(query);
 		//console.log(result.rows);
